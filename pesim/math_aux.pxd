85df270803d0426a128055137af52f54b5397887
tefx <zhaomeng.zhu@gmail.com>
2021-02-22 16:23:35 +0800

    new d2l and l2d

--- pesim/math_aux.pxd
+++ pesim/math_aux.pxd
@@ -1,11 +1,10 @@
 from libc.stdint cimport int64_t
 
-cdef double _EPSILON
-# cdef double _EPSILON_RECIPROCAL
-
-cpdef bint feq(double a, double b)
-cpdef bint flt(double a, double b)
-cpdef bint fle(double a, double b)
-
 cpdef int64_t d2l(double x)
 cpdef double l2d(int64_t x)
+
+cpdef bint time_eq(double a, double b)
+cpdef bint time_le(double a, double b)
+cpdef bint time_lt(double a, double b)
+cpdef bint time_ge(double a, double b)
+cpdef bint time_gt(double a, double b)
--- pesim/math_aux.pyx
+++ pesim/math_aux.pyx
@@ -1,33 +1,24 @@
-from libc.math cimport ceil
+from libc.math cimport round
 
-_EPSILON = 1e-4
-EPSILON = _EPSILON
-
-cdef double _EPSILON1 = _EPSILON * 10
-cdef double _EPSILON1_RECIPROCAL = 1.0 / _EPSILON1
-
-# EPSILON1 = _EPSILON1
-# EPSILON1_RECIPROCAL = _EPSILON1_RECIPROCAL
-
-cpdef bint feq(double a, double b):
-    return -_EPSILON < a - b < _EPSILON
-
-cpdef bint flt(double a, double b):
-    return a + _EPSILON <= b
-
-cpdef bint fle(double a, double b):
-    return a < b + _EPSILON
+cdef double EPSILON_RECIPROCAL = 1e3
 
 cpdef int64_t d2l(double x):
-    cdef int64_t y
-    x *= _EPSILON1_RECIPROCAL
-    y = <int64_t> (x + 1)
-    return y - <int64_t> (y - x)
+    return <int64_t>round(x * EPSILON_RECIPROCAL)
 
 cpdef double l2d(int64_t x):
-    return <double> (x * _EPSILON1)
+    return (<double>x) / EPSILON_RECIPROCAL
 
-# cdef double d2d(double x):
-#     # return <int64_t>(x * _EPSILON1_RECIPROCAL + .999) * _EPSILON1
-#     return (_LONG_MAX_L - <int64_t>(_LONG_MAX_D - x * _EPSILON1_RECIPROCAL)) * _EPSILON1
+cpdef bint time_eq(double a, double b):
+    return d2l(a) == d2l(b)
 
+cpdef bint time_le(double a, double b):
+    return d2l(a) <= d2l(b)
+
+cpdef bint time_lt(double a, double b):
+    return d2l(a) < d2l(b)
+
+cpdef bint time_ge(double a, double b):
+    return d2l(a) >= d2l(b)
+
+cpdef bint time_gt(double a, double b):
+    return d2l(a) > d2l(b)
